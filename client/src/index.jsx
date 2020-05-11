44c313556d4aaefc8e9b8df6150e400cfd57b7b0
lcukerd <harshit05081997@gmail.com>
2020-05-11 18:15:43 +0530

    Removed useless react code

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,6 @@
-import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Player from './components/player'
-import * as serviceWorker from './serviceWorker';
 import openSocket from 'socket.io-client';
 
 const socket = openSocket('http://localhost:1997');
@@ -10,4 +8,3 @@ socket.on('private', data => {
   console.log(`Got a Secret message ${data}`);
 })
 ReactDOM.render(<Player />, document.getElementById('root'));
-serviceWorker.unregister();
