229ac7ff5f89bede75642aed139d5ca5c8cd0cd6
lcukerd <harshit05081997@gmail.com>
2020-05-17 00:25:15 +0530

    Server and client working from same webserver

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const path = require('path');
 
 server.listen(1997);
 let connectedRooms = {};
 
-app.get('/', (req, res) => {
-    res.send('Hello');
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
 
 io.on('connection', socket => {
