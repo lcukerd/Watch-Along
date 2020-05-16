9873ea1c467700955e9bb8dae012d8d0704afb33
lcukerd <harshit05081997@gmail.com>
2020-05-17 00:57:54 +0530

    Small prod bug fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const express = require('express')
+const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const path = require('path');
