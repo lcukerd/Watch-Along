c3abade112ecd85c69c816a7292c654125262d11
lcukerd <harshit05081997@gmail.com>
2020-05-19 15:10:15 +0530

    Remove path

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,8 +4,7 @@ import Room from './components/room'
 import RoomieName from './components/roomieName';
 
 const roomId = window.location.search.replace("?id=", '');
-const path = window.location.pathname;
 console.log(roomId);
 
-if (path === '/room') ReactDOM.render(<RoomieName roomId={roomId} />, document.getElementById('root'));
+if (roomId) ReactDOM.render(<RoomieName roomId={roomId} />, document.getElementById('root'));
 else ReactDOM.render(<Room />, document.getElementById('root'));
